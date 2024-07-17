e0e73cc5e5c98d991c9ba6fd34f00a5bd9b53aa8
Shivam Garg <37806624+shivamgarg17@users.noreply.github.com>
2024-07-17 23:59:51 +0530

    file added3

--- k6_setup.sh
+++ k6_setup.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-brew install k6
+set -ex
+
+ apt-get update
+ apt-get install dirmngr --install-recommends
+ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
+echo "deb https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
+ apt-get update
+ apt-get install k6
